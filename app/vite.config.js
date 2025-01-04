8f6f74ebf6035765d284cc96d722c4095d7b2cf1
Belén Nuñez <gabinuniez237@gmail.com>
2025-01-04 19:52:25 -0300

    cambios

--- app/vite.config.js
+++ app/vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
-  build: {
-    outDir: 'dist',  // Esto asegura que Vite cree el directorio 'dist'
-  }
-});
+  plugins: [react()],
+})
